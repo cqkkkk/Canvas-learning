59436be1c6828e5c9ff330ecb51373cd1dc385b1
cqk <1296437659@qq.com>
2018-08-29 20:45:35 +0800

    add rotate

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function draw() {
     var canvas = document.getElementById('tutorial1');
     if (!canvas.getContext) return;
     var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, 500, 500);
     ctx.fillStyle = "black";
     ctx.beginPath();
     ctx.fillRect(20, 20, 100, 100);
     ctx.closePath();
 
-
+    //心跳
     ctx.beginPath();
     ctx.moveTo(150, 150);
     ctx.lineTo(170, 150);
@@ -23,5 +24,40 @@ function draw() {
     ctx.lineTo(235, 150);
     ctx.lineTo(250, 150);
     ctx.stroke();
+
+    //太阳
+    ctx.fillStyle = "yellow";
+    ctx.beginPath();
+    ctx.arc(300, 300, 50, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.save();
+
+    //轨迹
+    ctx.translate(300, 300);
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.arc(0, 0, 100, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.stroke();
+
+    let time = new Date();
+    ctx.rotate(2 * Math.PI / 6 * time.getSeconds() + 2 * Math.PI / 6000 * time.getMilliseconds());
+    ctx.translate(100, 0);
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(0, 0, 20, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+
+    requestAnimationFrame(draw);
+
 }
-draw();
+function init() {
+    window.onload = function () {
+        draw();
+    }
+}
+
+init();
